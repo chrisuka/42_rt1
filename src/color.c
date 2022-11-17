b3b197555f1a3cce5af9cb6e7a5ad40f28875439
ikarjala <ikarjala@student.hive.fi>
2022-11-17 20:10:51 +0200

    update color, renderutils

--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:16:12 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/11/17 17:35:50 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/11/17 19:33:34 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ unsigned int	to_rgb(t_color c)
 		(unsigned int)(c.r * 255) << (__CHAR_BIT__ * 2) |
 		(unsigned int)(c.g * 255) << (__CHAR_BIT__ * 1) |
 		(unsigned int)(c.b * 255) << (__CHAR_BIT__ * 0)
-		& (0xFF << __CHAR_BIT__ * 3)
+		& (0xFF << (__CHAR_BIT__ * 3))
 	);
 }
--- src/render_utils.c
+++ src/render_utils.c
@@ -6,38 +6,40 @@
 /*   By: ikarjala <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:16:56 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/11/17 18:45:01 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/11/17 19:58:58 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/rtv1.h"
 
+#if 0
 /* Return index position of pixel at (x,y)
 */
 static inline unsigned int	px_at(int x, int y)
 {
-	return (y * WIN_H + x * sizeof(int));
+	return (y * WIN_H + x);
 }
 
 /* Set the RGB color value of a pixel in the video buffer.
 */
-void	set_color(t_sdl *sdl, int x, int y, unsigned int color)
-//void	set_color(t_sdl *sdl, int x, int y)
+void	set_pixel(t_sdl *sdl, int x, int y, unsigned int color)
 {
-	const double	p;
+	sdl->vbuf[px_at(x, y)] = color;
+}
+#endif
+
+void	set_color(t_sdl *sdl, int x, int y)
+{
+	double	p;
 
 	if (sdl->light.new_inten > 1)
 		sdl->light.new_inten = 1;
 	p = sdl->light.new_inten;
-# if 0
 	SDL_SetRenderDrawColor(sdl->rend,
 			sdl->obj[0].col.rgb[0] * p,
 			sdl->obj[0].col.rgb[1] * p,
 			sdl->obj[0].col.rgb[2] * p, 255);
 	SDL_RenderDrawPoint(sdl->rend, x, y);
-# else
-	sdl->vbuf[px_at(x, y)] = color;
-# endif
 }
 
 // SDL_RenderClear == mlx_clear_window
