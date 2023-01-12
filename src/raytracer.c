9c4a57c8375601bd8ee9f53c32653a5bf08dfcc9
Emma Kantanen <ekantane@c1r6p7.hive.fi>
2023-01-12 16:41:59 +0200

    added cast shadows

--- src/raytracer.c
+++ src/raytracer.c
@@ -6,13 +6,21 @@
 /*   By: ekantane <ekantane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 21:23:06 by ikarjala          #+#    #+#             */
-/*   Updated: 2023/01/09 13:56:27 by ekantane         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:40:52 by ekantane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-t_obj*	find_nearest(t_scene *ctx, t_ray ray, double *min_t)
+static inline t_ray	project_ray_from_light(t_light light, t_vec hit_point)
+{
+	return ((t_ray){
+		.orig = hit_point,
+		.dir = vec_norm(vec_sub(light.pos, hit_point))
+	});
+}
+
+static inline t_obj*	find_nearest(t_scene *ctx, t_ray ray, double *min_t)
 {
 	t_obj	*nearest;
 	double	t;
@@ -21,10 +29,10 @@ t_obj*	find_nearest(t_scene *ctx, t_ray ray, double *min_t)
 	nearest = NULL;
 	t = 0;
 	i = 0;
-	while (i <= 2)
+	while (i <= 3)
 	{
 		t = intersect (ray, ctx->obj[i]);
-		if (t > 0 && t < *min_t)
+		if (t > EPS && t < *min_t)
 		{
 			*min_t = t;
 			nearest = &ctx->obj[i];
@@ -65,21 +73,27 @@ t_rgbf	raytrace(t_scene *ctx, t_ray ray)
 {
 	t_rgbf	c;
 	t_obj	*nearest;
+	t_obj	*nearest_light;
 	t_rt	rt;
+	double	light_t;
+	t_ray	ray_light;
 
 	rt.min_t = INFINITY;
 	nearest = find_nearest(ctx, ray, &rt.min_t);
-
 	if (nearest)
 		c = nearest->color;
 	else
 		return ((t_rgbf){0, 0, 0});
 	rt.hit_point = vec_sum (ray.orig, vec_scale (ray.dir, rt.min_t));
-	c = cmul (c, fmin(1.0L, ctx->ambient + get_intensity (
+	ray_light = project_ray_from_light (ctx->light, rt.hit_point);
+	light_t = vec_len(vec_sub(ctx->light.pos, rt.hit_point));
+	nearest_light = find_nearest(ctx, ray_light, &light_t);
+	if (nearest_light == NULL)
+		c = cmul (c, fmin(1.0L, ctx->ambient + get_intensity (
 				rt.hit_point,
 				get_object_normal (ray.dir, rt.hit_point, nearest, ray.orig),
 				ctx->light, nearest)));
-
-	// TODO: specular
+	else
+		c = cmul (c, fmin(1.0L, ctx->ambient));
 	return (c);
 }
